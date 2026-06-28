614b5d7c0e1c1efb1d5dff058337e1dffe388cd4
AhamedAlisha786 <n190493@rguktn.ac.in>
2026-06-28 23:16:46 +0530

    the new

--- BlogWebiste/Backend/src/main/java/com/example/LoanApp/Repository/UserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.LoanApp.Repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.example.LoanApp.Entity.RegisterEntity;
-
-public interface UserRepository extends JpaRepository<RegisterEntity, Long> {
-	RegisterEntity findByEmail(String Email);
-}
